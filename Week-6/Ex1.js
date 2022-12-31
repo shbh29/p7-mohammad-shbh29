305584fcd80969e918e3136b7d7fe4d2664ff3f9
Shubham Singh <shbh29@gmail.com>
2022-12-31 15:03:32 +0530

    week 7 ex 1: max sum of sequence of array.

---
+++ Week-6/Ex1.js
@@ -0,0 +1,19 @@
+function getMaxSum(arr) {
+    let maxSum = 0;
+    let currentSum = 0;
+    for (let e of arr) {
+        currentSum += e;
+        if ( currentSum < e) {
+            currentSum = e;
+        }
+        if (maxSum <= currentSum) {
+            maxSum = currentSum;
+        } 
+    }
+    return maxSum;
+}
+
+let arr = [2,4,-10,7,4,-3,2,2];
+// arr = [1,2,3,4,-10];
+
+console.log("max sum from sequence of array is: ",getMaxSum(arr));
